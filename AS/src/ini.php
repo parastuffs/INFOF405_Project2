781d7183f57649dc682779338e77a493e5686c6d
Gilles Degols <gdegols@ulb.ac.be>
2013-12-01 01:43:46 +0100

    AS - ini.php - changes for linux.

--- AS/src/ini.php
+++ AS/src/ini.php
@@ -24,10 +24,18 @@ if(!file_exists('.htaccess'))
     {
         //We create the htaccess
         $path = realpath('index.php');
-        $ori = $path;
-        $path = explode('\\',$path);
-        array_pop($path);
-        $path = implode('\\',$path);
+        $inf = explode('/',$path);
+        if(isset($inf[1]))
+        {//linux
+            array_pop($inf);     
+            $path = implode('/',$inf);
+        }
+        else
+        {//windows
+            $path = explode('\\',$path);   
+            array_pop($path);     
+            $path = implode('\\',$path);
+        }
         $fic = fopen('.htaccess','w');
         fputs($fic, 'AuthName "Administration: see readme.txt"
                     AuthType Basic
