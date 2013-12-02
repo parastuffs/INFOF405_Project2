85eb574c90fcec09f4a51cf96ce40a220dd0a166
Quentin Delhaye <qudelhay@ulb.ac.be>
2013-12-02 14:01:44 +0100

    deletes clientcon.java

--- src/authorisationServer/AuthorisationServer.java
+++ src/authorisationServer/AuthorisationServer.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
