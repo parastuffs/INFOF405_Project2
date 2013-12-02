b7e50f94daeab158424967d06cfac8fd8d07475e
Gilles Degols <gdegols@ulb.ac.be>
2013-12-02 02:18:15 +0100

    AS (java) - En cours de creation cf. commentaire DatabasePassword.java -> Il y a deux TODO indispensable à faire, Il faut regarder dans AuthorisationServer.java de Quentin car il s'est servi d'AES.
    User -> verifier que les trucs de requête sont plus ou moins cohérent,
    juste la syntaxe en générale en 2 minutes, car je me sers partout de la
    meme. J'ai copié tres fortement de la blackboard :p. C'est la premiere
    fois que j'utilise sql avec java donc bon :/.

---
+++ src/authorisationServer/DatabasePassword.java
@@ -0,0 +1,159 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+
+public class DatabasePassword {
+	
+	private String mainKey="";
+	
+	public DatabasePassword()
+	{
+		this.takeMainPassword();
+	}
+	
+	/**
+	 * Take the content of the file which contains a important password for all the passwords used by the webinterface and analyze it, then give back the information
+	 * @return String or empty if nothing found.
+	 */
+	private String takeMainPassword()
+	{
+		//We take the file and analyze it
+		String t = this.takeFile("AS/src/php/Crypt.class.php");
+		String inf[];
+		
+		inf = t.split("const SPECIFIC_SALT \\= \"");
+		if(inf.length <= 1)
+			return "";
+		
+		inf = inf[1].split("\";");
+		t = inf[0];
+		
+		//We have the key :).
+		this.mainKey = t;
+		return t;
+	}
+	
+	/**
+	 * Take the content of a file
+	 * @param link the file to take
+	 * @return
+	 */
+	private String takeFile(String link)
+	{
+		String line, text="";
+		char specialLetter = 146;//We sometimes read into the file the character ' (apostrophe) which isn't correctly read, so we have to change the character read (146) 
+				
+		//We take the text inside
+		try
+		{			
+			RandomAccessFile f = new RandomAccessFile(link,"r");
+			
+			while((line = f.readLine()) != null)
+			{		
+				line = line.replace(specialLetter, '\'');
+				line = line.replace('\t', ' ');
+				line = line.replace('\r', ' ');
+				
+				text += line;				
+			}			
+				
+			f.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Problem to open the file");
+			e.printStackTrace();
+		}
+		
+		return text;
+	}
+	
+	/**
+	 * Decrypt the information if there is a password used, if not, we just send back the same text
+	 * @param type String the information type (WS, publicKey, privateKey, etc.)
+	 * @param texte String
+	 * @param salt String the salt used
+	 * @return texte
+	 */
+	public String decrypt(String type, String text, String salt)
+	{
+		String pass="";
+		
+		//We check if there is a password
+		pass = this.password(type, salt);
+		
+		if(pass.isEmpty())
+		{//There is nothing to do...
+			return text;
+		}
+		else
+		{//We decrypt the text
+			return "";//TODO             IMPORTANT!!!!! Decryptage rijndael-128 mode cfb à mettre en place (si probleme de mode ou quoi, je peux encore le changer)
+		}
+	}
+	
+
+	/**
+	 * Crypt the information if there is a password used, if not, we just send back the same text
+	 * @param type String the information type (WS, publicKey, privateKey, etc.)
+	 * @param texte String
+	 * @param salt String the salt to use
+	 * @return texte
+	 */
+	public String crypt(String type, String text, String salt)
+	{
+		String pass="";
+		
+		//We check if there is a password
+		pass = this.password(type, salt);
+		
+		if(pass.isEmpty())
+		{//There is nothing to do...
+			return text;
+		}
+		else
+		{//We crypt the text
+			
+            return "";//TODO             IMPORTANT!!!!! Cryptage rijndael-128 mode cfb à mettre en place (si probleme de mode ou quoi, je peux encore le changer)
+		}
+	}
+	
+	/**
+	 * Give back the corresponding password or nothing if no password
+	 */
+	private String password(String type, String salt)
+	{
+		Map<String, String> pass = new HashMap<String, String>();
+		pass.put("WS1", this.sha1("564zecv41zFEFEZ"+this.mainKey+"f4fl"+salt+"p^dz^l^p)àç1!à!èç'"));
+		pass.put("WS2", this.sha1("564zecv42zFEFEZ"+this.mainKey+"f4fl"+salt+"p^dz^l^p)àç2!à!èç'"));
+		pass.put("publicKey", this.sha1("^à)çà!çèç)"+this.mainKey+"§FEFEZµù"+salt+"$55878zefFE"));
+		pass.put("privateKey", this.sha1("^àù$$^"+salt+"ù$ùù$$ùù$czq$ù$ùcqsd$ùq"+this.mainKey+"$ùdqscv"+salt+"$558"));
+		pass.put("sessionKey", this.sha1("^àù$$^"+salt+"ù56zef169ez1f56eç!è!ç"+this.mainKey+"à)$^ùµfezfezfezgecv"+salt+"$558"));
+		pass.put("keyOrigin", this.sha1("à)àç)&é)éàz"+this.mainKey+"gecv$558"));
+		pass.put("keyDestination", this.sha1("à)àçF4fez9E"+this.mainKey+"ecv$558"));
+		pass.put("passwordUsername", this.sha1("à45fzae"+this.mainKey+"zefzfe0979"+this.mainKey+"8IKpml$558"));
+		pass.put("hashidUsername", this.sha1("à)àçF45àè§è!"+salt+"èfez9Eec"+this.mainKey+"v$558"));
+		pass.put("hashedId", this.sha1("àEFZEF§è!"+salt+"èfezrthtr"+this.mainKey+"cv$558"));
+		
+		if(!pass.containsKey(type))
+		{
+			System.out.println("[Pas de pass] Type: "+type+", Salt:"+salt);
+			return "";
+		}
+		else
+		{
+			return pass.get(type);
+		}
+	}
+	/**
+	 * make a sha1 hash on a given text 
+	 */
+	public String sha1(String text)
+	{
+		return DigestUtils.sha1Hex(text);
+	}
+}
---
+++ src/authorisationServer/User.java
@@ -0,0 +1,240 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+
+
+public class User
+{
+	private static User INSTANCE = null;
+	private static String HOST = "localhost";
+	private static String PORT = "3306";
+	private static String USER = "root";
+	private static String PASS = "";
+	  
+	private static Statement stmt;
+	private static PreparedStatement prepStmt;
+	private static Connection con;
+	
+	private String userId="";//will be 'CL'+idClient or 'WS1' or 'WS2' or 'AS'	
+	private String publicKey="";
+	private String privateKey="";
+	private boolean accessWS1=false;
+	private boolean accessWS2=false;
+	private String saltAsymKey="";
+	private String saltSessionKey="";
+	private String saltUser="";
+	
+	private DatabasePassword dbPassword;
+	
+	public static User getInstance()
+	{
+		if(INSTANCE==null)		
+			INSTANCE = new User();
+		
+		return INSTANCE;
+	}
+		   
+	private User()
+	{
+		this.dbPassword = new DatabasePassword();
+		
+		String url = "jdbc:mysql://"+HOST+":"+PORT+"/User?createDatabaseIfNotExist=true";
+		User.con = null;
+		User.stmt = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");//load the driver
+		    con = DriverManager.getConnection(url,USER,PASS); //connect to mysql
+		    this.createTables(); //create tables if not exist :
+		    System.out.println("Connected to DB successfully"); //DEBUG
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+		    e.printStackTrace();
+		}		 
+	}
+	
+	private void closeStmt()
+	{
+		if(stmt!=null)
+		{
+			try 
+			{
+		       stmt.close();
+		    } catch (SQLException e) {
+		    	e.printStackTrace();
+		    }
+		}
+		
+		if(prepStmt!=null)
+		{
+			try
+			{
+		       prepStmt.close();
+		    } catch (SQLException e) {
+		         e.printStackTrace();
+		    }
+		 }
+	}
+	
+	public void closeConnection()
+	{
+		if(con!=null) 
+		{
+			try 
+			{
+				con.close();
+				System.out.println("Closed DB connection successfully");
+			} catch (SQLException e) {
+		         e.printStackTrace();
+			}
+	     }	
+	}
+	
+	private boolean createTables()
+	{
+		return true;
+	}
+	
+	/**
+	 * Take all the information for the user (access to WS1 or WS2 valid), only called if this is client
+	 * @param userId (int! not 'CL'+id)
+	 * @return boolean
+	 */
+	public boolean getUser(int userId) 
+	{		
+		String temp="";
+		//We take all the information directly
+		String sql = "SELECT u.* FROM info405.user u WHERE u.id="+userId;
+		
+		//    System.out.println(sql); //DEBUG
+		ResultSet rs;
+		try 
+		{
+			stmt = con.createStatement();
+		    rs = stmt.executeQuery(sql);
+		    		    	
+		    //We take the salt
+		    this.saltUser = rs.getString("salt");
+		    
+		    //We take the WS1
+		    temp = rs.getString("WS1");
+		    temp = this.dbPassword.decrypt("WS1",temp, this.saltUser);
+		    if("1".equals(temp))
+		    	this.accessWS1 = true;
+		    else
+		    	this.accessWS1 = false;
+		    
+
+		    //We take the WS2
+		    temp = rs.getString("WS2");
+		    temp = this.dbPassword.decrypt("WS2",temp, this.saltUser);
+		    if("1".equals(temp))
+		    	this.accessWS2 = true;
+		    else
+		    	this.accessWS2 = false;  		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		    return false;
+		} finally {
+			this.closeStmt();  
+		}
+		     
+		return true;
+	}
+	
+	/**
+	 * Take the information for the asymmetric keys from the db
+	 * @param owner String ('CL'+id or 'AS' or 'WS1' or 'WS2')
+	 * @return boolean
+	 */
+	public boolean getAsymKey(String owner)
+	{
+		String temp="";
+		
+		//We take all the information directly
+		String sql = "SELECT a.* FROM info405.asymkey u WHERE validity=1 AND a.owner="+owner;
+		
+		//    System.out.println(sql); //DEBUG
+		ResultSet rs;
+		try 
+		{
+			stmt = con.createStatement();
+		    rs = stmt.executeQuery(sql);
+		    		    	
+		    //We take the salt
+		    this.saltAsymKey = rs.getString("salt");
+		    
+		    //We take the public key
+		    temp = rs.getString("publicKey");
+		    this.publicKey = this.dbPassword.decrypt("publicKey", temp, this.saltAsymKey);
+
+		    //We take the private key (it does not exist always)
+		    if("AS".equals(owner))
+		    {
+		    	temp = rs.getString("privateKey");
+		    	this.privateKey = this.dbPassword.decrypt("privateKey", temp, this.saltAsymKey);
+		    }
+		    
+		} catch (SQLException e) {
+			e.printStackTrace();
+		    return false;
+		} finally {
+			this.closeStmt();  
+		}
+		     
+		return true;
+	}
+	
+	/**
+	 * Take the session key for AS or WS (always the same) and give it back
+	 * @param owner String ('CL'+id or 'AS' or 'WS1' or 'WS2')
+	 * @return String
+	 */
+	public String getSessionKeyServer(String owner)
+	{
+		return "";
+	}
+	
+	/**
+	 * Insert a new session key into the db for the current user
+	 * @param value String well, we could put an empty string as we won't this key again...
+	 * @param boolean 
+	 */
+	public boolean insertNewSessionKey()
+	{
+		return true;
+	}
+	
+	/**
+	 * Revoke all the old session keys for the specified destination with the current user
+	 * @param destination String
+	 * @return
+	 */
+	public boolean revokeOldSessionKeys(String destination)
+	{
+		return true;
+	}
+
+	
+	/**
+	 * Just says if the user can access to the WS1
+	 * @return boolean
+	 */
+	public boolean getAccessWS1()
+	{
+		return this.accessWS1;
+	}
+	
+	/**
+	 * Just says if the user can access to the WS2
+	 * @return boolean
+	 */
+	public boolean getAccessWS2()
+	{
+		return this.accessWS2;
+	}
+}
